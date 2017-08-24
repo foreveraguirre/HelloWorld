4d15100d25bb1c8af61bd9c99461b567d7c0a5c6
foreveraguirre <foreveraguirre@gmail.com>
2017-08-24 16:14:08 -0500

    initial commit

---
+++ src/com/hcl/Main.java
@@ -0,0 +1,15 @@
+package com.hcl;
+
+public class Main {
+	
+	String name = "Efrain";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Main m = new Main();
+		
+		System.out.println(m.name + " Hello World!!");
+	}
+
+}
